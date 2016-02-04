1b0dcbeaa25a61dad0e55a865dc4bd99119bc712
vladiibine <vlad.ardelean@3pillarglobal.com>
2016-02-04 12:40:42 +0200

    parameters to make XOR converge extremely fast

--- neat_xor/xor.py
+++ neat_xor/xor.py
@@ -7,41 +7,38 @@ import sys
 xor_inputs = [[0, 0], [0, 1], [1, 0], [1, 1]]
 xor_outputs = [0, 1, 1, 0]
 
-simpler_inputs = [[0], [1], [2], [3], [4], [5], [6], [7]]
-simpler_outputs = [0, 1, 2, 3, 4, 5, 6, 7]
-
-def simpler_eval_fitness(genomes):
-    for g in genomes:
-        net = nn.create_feed_forward_phenotype(g)
-
-        error = 0.0
-        for inputs, expected in zip(simpler_inputs, simpler_outputs):
-            output = net.serial_activate(inputs)
-            error += (output[0] - expected) ** 2
-        g.fitness = 1-error
-#        import ipdb; ipdb.set_trace()
-
 def eval_fitness(genomes):
     for g in genomes:
         net = nn.create_feed_forward_phenotype(g)
 
-        error = 0.0
-        for inputs, expected in zip(xor_inputs, xor_outputs):
-            # Serial activation propagates the inputs through the entire network.
-            output = net.serial_activate(inputs)
-            error += (output[0] - expected) ** 2
-
         # When the output matches expected for all inputs, fitness will reach
         # its maximum value of 1.0.
-        g.fitness = 1 - error
+        g.fitness = get_fitness(net.serial_activate)
+
+def get_fitness(function):
+    error = 0.0
+    for inputs, expected in zip(xor_inputs, xor_outputs):
+        # Serial activation propagates the inputs through the entire network.
+        output = function(inputs)
+        error += (output[0] - expected) ** 2
+    return 1 - error
 
 
 pop = population.Population('config')
-#import ipdb; ipdb.set_trace()
 num_epochs = int(sys.argv[1])
 
+def get_results(pop):
+    net = nn.create_feed_forward_phenotype(pop.most_fit_genomes[-1])
+    results = [net.serial_activate(pair) for pair in xor_inputs]
+    return results, net.serial_activate
+
 def old_more_complex_stuff():
-    pop.epoch(eval_fitness, num_epochs)
+#    pop.epoch(eval_fitness, num_epochs)
+    while True:
+        try:
+            pop.epoch(eval_fitness, num_epochs)
+        except KeyboardInterrupt:
+            import pdb; pdb.set_trace()
 
     print('Number of evaluations: {0}'.format(pop.total_evaluations))
 
@@ -57,24 +54,6 @@ def old_more_complex_stuff():
         output = winner_net.serial_activate(inputs)
         print("expected {0:1.5f} got {1:1.5f}".format(expected, output[0]))
 
-
-
-def new_and_simpler_stuff():
-    pop.epoch(simpler_eval_fitness, num_epochs)
-
-    print('Number of evaluations: {0}'.format(pop.total_evaluations))
-
-# Display the most fit genome.
-    print('\nBest genome:')
-    winner = pop.most_fit_genomes[-1]
-    print(winner)
-
-# Verify network output against training data.
-    print('\nOutput:')
-    winner_net = nn.create_feed_forward_phenotype(winner)
-    for inputs, expected in zip(simpler_inputs, simpler_outputs):
-        output = winner_net.serial_activate(inputs)
-        print("expected {0:1.5f} got {1:1.5f}".format(expected, output[0]))
 
 #run just the new stuff
 #new_and_simpler_stuff()
